ea6c0913a3a2291592c94afe6fe35aa750885fa5
Eduardo Galeano <cegard689@gmail.com>
2023-03-13 11:21:38 -0500

    Home page added.

---
+++ src/components/imageButtonLink.jsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+const ImgWithLinkButton = styled.div`
+  position: relative;
+
+  img {
+    height: auto;
+    width: 600px;
+    object-fit: contain;
+  }
+
+  button {
+    text-transform: uppercase;
+    width: 120px;
+    position: absolute;
+    top: 75%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    background-color: #555;
+    color: white;
+    font-size: 16px;
+    padding: 0.75rem 1.5rem;
+    border: none;
+    cursor: pointer;
+  }
+`;
+
+export default function ImageButtonLink({imgSource, altText, linkTo, btnText}) {
+  return(
+    <ImgWithLinkButton>
+      <a href={linkTo}>
+        <img src={imgSource} alt={altText} />
+      </a>
+      <button class="btn">{btnText}</button>
+    </ImgWithLinkButton>
+  );
+} 
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,32 @@
 import Header from "../components/header";
+import styled from "styled-components";
+import ImageButtonLink from "../components/imageButtonLink";
 
+const CatsImageLinks = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-left: auto;
+  margin-right: auto;
+`;
 
 export default function Home() {
   return(
-    <Header/>
+    <>
+      <Header/>
+      <CatsImageLinks>
+        <ImageButtonLink
+          imgSource="/static/images/1.jpg"
+          altText="women's shoes"
+          linkTo="/category/women"
+          btnText="mujeres"
+        />
+        <ImageButtonLink
+          imgSource="/static/images/11.jpg"
+          altText="men's shoes"
+          linkTo="/category/men"
+          btnText="hombres"
+        />
+      </CatsImageLinks>
+    </>
   );
 }
